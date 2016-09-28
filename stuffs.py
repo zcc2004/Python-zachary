19ea199e4ff1a1879975b2df3b0d161e980ceb9c
zcc2004 <rockinboppingheads@icloud.com>
2016-09-28 17:54:23 -0400

    fmhcr
    thxrf

---
+++ stuffs.py
@@ -0,0 +1,15 @@
+directory = {'zoc':9999,'zac':99999999999}
+answer = input('whos grad u want')
+
+if answer == 'search':
+    answer = input('wot yo dou')
+    search(answer)
+elif answer == 'add':
+    answer1 = input('You Swine, You vulgar little maggot. Like we say in texas I bet you couldnt pour piss out of a boot without instructions on the heel.You are a canker, a sore that wont go away.')
+    answer2 = input('YOYOYOYYOYYOOYOYOYOOYOYOYOOY')
+    add(answer1, answer2)
+elif answer == 'delete':
+    answer = input('who want to dlete')
+    delete(answer)
+else:
+    print('fu8fieruuvgsernhjhjjjjjjjjjjjjjjjjjjjjgu,k gh g g g g9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9p;[-9
